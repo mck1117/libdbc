3027e8cb54eb11c09e52922689389620ca720550
Matthew Kennedy <matthewkennedy@outlook.com>
2024-03-28 18:40:57 -0700

    don't re-test charconv

--- test/TestParsing.cpp
+++ test/TestParsing.cpp
@@ -4,48 +4,6 @@
 
 #include <fstream>
 
-/*TEST(Parsing, ParseInt)
-{
-    EXPECT_EQ(0, libdbc::util::ParseInt("0"));
-    EXPECT_EQ(0, libdbc::util::ParseInt("-0"));
-    EXPECT_EQ(1, libdbc::util::ParseInt("1"));
-    EXPECT_EQ(-1, libdbc::util::ParseInt("-1"));
-
-    EXPECT_EQ(10, libdbc::util::ParseInt("10"));
-    EXPECT_EQ(100000, libdbc::util::ParseInt("100000"));
-    EXPECT_EQ(-10, libdbc::util::ParseInt("-10"));
-    EXPECT_EQ(-100000, libdbc::util::ParseInt("-100000"));
-
-    EXPECT_EQ(12345, libdbc::util::ParseInt("12345"));
-    EXPECT_EQ(1234567890, libdbc::util::ParseInt("01234567890"));
-}
-
-TEST(Parsing, ParseFloat)
-{
-    EXPECT_EQ(0, libdbc::util::ParseFloat("0"));
-    EXPECT_EQ(1, libdbc::util::ParseFloat("1"));
-    EXPECT_EQ(-1, libdbc::util::ParseFloat("-1"));
-
-    EXPECT_EQ(10, libdbc::util::ParseFloat("10"));
-    EXPECT_EQ(100000, libdbc::util::ParseFloat("100000"));
-    EXPECT_EQ(-10, libdbc::util::ParseFloat("-10"));
-    EXPECT_EQ(-100000, libdbc::util::ParseFloat("-100000"));
-
-    EXPECT_EQ(12345, libdbc::util::ParseFloat("12345"));
-    EXPECT_EQ(1234567890, libdbc::util::ParseFloat("01234567890"));
-
-
-    EXPECT_FLOAT_EQ(0.1f, libdbc::util::ParseFloat("0.1"));
-    EXPECT_FLOAT_EQ(123.45f, libdbc::util::ParseFloat("123.45"));
-    EXPECT_FLOAT_EQ(0.0001f, libdbc::util::ParseFloat("0.0001"));
-    EXPECT_FLOAT_EQ(0.00048828125f, libdbc::util::ParseFloat("0.00048828125"));
-    EXPECT_FLOAT_EQ(0.11111111f, libdbc::util::ParseFloat("0.111111111111111111111111"));
-
-    EXPECT_FLOAT_EQ(-0.1f, libdbc::util::ParseFloat("-0.1"));
-    EXPECT_FLOAT_EQ(-123.45f, libdbc::util::ParseFloat("-123.45"));
-    EXPECT_FLOAT_EQ(-0.0001f, libdbc::util::ParseFloat("-0.0001"));
-}*/
-
 static std::unique_ptr<libdbc::Dbc> ParseDbcFile(const char* path)
 {
     std::ifstream f(path);
