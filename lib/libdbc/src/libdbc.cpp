7751afe969d73d0ed6dfdf34e757ed57326d14b4
Matthew Kennedy <matthewkennedy@outlook.com>
2024-04-07 00:39:59 -0700

    static

--- lib/libdbc/src/libdbc.cpp
+++ lib/libdbc/src/libdbc.cpp
@@ -58,7 +58,7 @@ void Dbc::Decode(const CanFrame& frame, DecoderFunc onDecoded) const
     }
 }
 
-uint64_t swap_uint64_t(uint64_t x) {
+static uint64_t swap_uint64_t(uint64_t x) {
     x = (x & 0x00000000FFFFFFFF) << 32 | (x & 0xFFFFFFFF00000000) >> 32;
     x = (x & 0x0000FFFF0000FFFF) << 16 | (x & 0xFFFF0000FFFF0000) >> 16;
     x = (x & 0x00FF00FF00FF00FF) << 8  | (x & 0xFF00FF00FF00FF00) >> 8;
